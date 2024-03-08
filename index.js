92fbfacb37c223a05eae26609c24c554f111be1f
$@#il <sahilsadekar249775@gmail.com>
2024-03-08 22:58:31 +0530

    added info

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
 
+
 app.use('/api',function(req, res, next) {
     console.log(req.url);
+    console.log('Middleware executed');
     next();
 })
 app.get('/:id',(req,res)=>{
-    console.log("GET request");
+    console.log("GET request");//printed twice because in network section it sending twice request for get 1 time and another for favicon.ico exploer it for doubts
     res.send("first get request...");
 });
 app.listen(3000);
